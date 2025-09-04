bd1801d84b9335dbaf1e4d75dd7ca6282da3bf7e
Gabriel Rossi <biel.invic@gmail.com>
2025-09-04 17:27:12 -0300

    Utilização do metodo get

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -3,12 +3,19 @@ public class LivroDeNotas{
     public void  setDisciplina(String d){
         Disciplina = d;
     }
-
+    
+    public String getDisciplina(){
+        return Disciplina;
+    }
     //Variavel de referencia
     private String Disciplina;
 
     public void ExibirMensagem(){
         System.out.println("Bem vindo ao livro de notas de "  + Disciplina);
+    }
+
+    public LivroDeNotas(){
+        System.out.println("Criando um objeto");
     }
 
 }
--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -6,10 +6,12 @@ public class TesteLivroDeNotas{
         
         LivroDeNotas Livro1 = new LivroDeNotas();
         Livro1.setDisciplina(Disciplina1);
+        System.out.println(Livro1.getDisciplina());
         Livro1.ExibirMensagem();
 
         LivroDeNotas Livro2 = new LivroDeNotas();
         Livro2.setDisciplina(Disciplina2);
+        System.out.println(Livro2.getDisciplina());
         Livro2.ExibirMensagem();
 
         //teste de alocação de variavel para entender o garbage colector
